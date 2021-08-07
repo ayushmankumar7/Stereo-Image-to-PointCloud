4a8b30f0aaeea9d669987d47007604790fc1d151
ayushmankumar7 <ayushmankumar7@gmail.com>
2021-08-07 22:03:38 +0530

    Disparity map visible

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 #include <string>
 #include <Eigen/Core>
 #include <pangolin/pangolin.h>
-#include <unisrd.h> 
+#include <unistd.h> 
 
 using namespace std; 
 using namespace Eigen; 
@@ -13,6 +13,7 @@ string right_image = "../images/right.png";
 
 void showPointCloud(const vector<Vector4d, Eigen::aligned_allocator<Vector4d>> &pointcloud);
 
+
 void showPointCloud(const vector<Vector4d, Eigen::aligned_allocator<Vector4d>> &pointcloud){
 
 	if (pointcloud.empty()){
@@ -21,12 +22,39 @@ void showPointCloud(const vector<Vector4d, Eigen::aligned_allocator<Vector4d>> &
 	}	
 
 	pangolin::CreateWindowAndBind("My 3D Reconstruction", 1024, 768); 
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
+	pangolin::OpenGlRenderState s_cam(
+		pangolin::ProjectionMatrix(1024, 768, 500, 500, 512, 389, 0.1, 1000),
+		pangolin::ModelViewLookAt(0, -0.1, -1.8, 0, 0, 0, 0.0, -1.0, 0.0)
+	);
 
+	pangolin::View &d_cam = pangolin::CreateDisplay()
+		.SetBounds(0.0, 1.0, pangolin::Attach::Pix(175), 1.0, -1024.0f / 768.0f)
+		.SetHandler(new pangolin::Handler3D(s_cam));
 
+	while (pangolin::ShouldQuit() == false){
 
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		d_cam.Activate(s_cam);
+		glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+		
+		glPointSize(2);
+		glBegin(GL_POINT);
+		for (auto &p: pointcloud) {
+			glColor3f(p[3], p[3], p[3]);
+			glVertex3d(p[0], p[1], p[2]);
+		}
+		glEnd();
+		pangolin::FinishFrame();
+		usleep(5000);
+
+	}
+	return;
 }
-
 
 
 
@@ -38,21 +66,23 @@ int main(int argc, char **argv){
 	
 	cv::Mat left  = cv::imread(left_image, 0);
 	cv::Mat right = cv::imread(right_image, 0);
+	
 	cv::Ptr<cv::StereoSGBM> sgbm = cv::StereoSGBM::create(0, 96, 9, 8 * 9 * 9, 32 * 9 * 9, 1, 63, 10, 100, 32);
 	cv::Mat disparity_sgbm, disparity;
 	sgbm->compute(left, right, disparity_sgbm);
 	disparity_sgbm.convertTo(disparity, CV_32F, 1.0 / 16.0f);
 
 	vector<Vector4d, Eigen::aligned_allocator<Vector4d>> pointcloud; 
+	
 
 	for (int v = 0; v < left.rows; v++){
 
 		for (int u = 0; u<left.cols; u++){
-			if (disparity.at<float>(u,v) <=0.0 || disparity.at<float>(u,v) >=96.0){
+			
+			if (disparity.at<float>(v, u) <=0.0 || disparity.at<float>(v, u) >=96.0){
 				continue;
 			}
-
-			Vector4d point(0, 0, 0, left.at<uchar>(u,v) / 255.0);
+			Vector4d point(0, 0, 0, left.at<uchar>(v, u) / 255.0);
 			
 			double x = (u - cx) / fx;
 			double y = (v - cy) / fy;
@@ -65,10 +95,11 @@ int main(int argc, char **argv){
 		
 		}
 	}
+	
 
 	cv::imshow("disparity", disparity / 96.0);
 	cv::waitKey(0);
-	showPointCloud(pointCloud);
+	showPointCloud(pointcloud);
 	return 0;
 }
 
